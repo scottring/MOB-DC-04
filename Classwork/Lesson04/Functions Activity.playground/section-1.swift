63e651d197bddfdcd0570783e570364ae220bdbd
Scott Kaufman <smkaufman@gmail.com>
2015-10-10 22:48:05 -0400

    Homework due Oct 11

--- Classwork/Lesson04/Functions Activity.playground/section-1.swift
+++ Classwork/Lesson04/Functions Activity.playground/section-1.swift
@@ -6,31 +6,104 @@
 // TODO: Write a function that prints out "Hello world!" 10 times
 
 func greeting () {
-    for i in 1...10 {
+    for _ in 1...10 {
         print("Hello World")
     }
 }
 
+greeting()
+
 // TODO: Write a function that prints out "Hit this line {number of iterations of the loop} times!" 20 times
 
+func hitThis () {
+    for i in 1...20 {
+        print("Hit this line \(i) times")
+    }
+}
 
+hitThis()
 
 // TODO: Write a function that accepts a string as a parameter. Print "Hello {value of string}!"
 
+func acceptString (name: String) {
+    print("Hello \(name)")
+}
+
+acceptString("Scott")
+
+
 // TODO: Write a function accepts a string optional. If the string optional exists, print "Hello {value of string}!". If it doesn't, print "Hello world!"
+
+var name: String?
+name = "Scott"
+func acceptStringOptional (name: String?) {
+    if (name != nil) {
+        print("Hello \(name!)")
+    } else {
+       print("Hello world!")
+    }
+}
+
+acceptStringOptional(name)
+
 
 // TODO: Write a function that takes one parameter, n, and returns an integer, the nth series in the fibonacci
 // sequence. The first fibonacci number is 0, the second is 1, the third is 1, the fourth is 2, fifth is 3, sixth is 5, etc. fibonacci numbers at sequence n are the sum of the n-1 and n-2 fibonacci number.
+
+/*func fibonacci (n: Int) -> Int {
+    var a: Int = 0
+    var b: Int = 1
+    var c: Int = 0
+    var x: Int = 0
+    
+    for i in 1...n {
+        if n ==
+    }
+        
+    }
+}*/
 
 // TODO: Write a function that calls the above function in order to print the sum of the first 20 fibonacci numbers.
 
 // TODO: Write a function that takes in a number and prints out whether it is prime, composite or neither.
 
+var number: Int
+number = 0
+func numberType (number: Int) {
+    for i in 2...number {
+        if number <= 0 {
+            print("neither")
+        } else if number % i == 0 {
+            print("composite")
+        } else if number % i > 0 {
+            print("prime")
+        }
+    }
+}
+
+numberType(4)
+
+
 // TODO: Write a function that prints out each of the first 20 fibonacci numbers and whether they are prime. (e.g. 0 is not prime or composite, 1 is prime, etc)
 
 // TODO: Write a function that takes in two numbers, a bill amount and an optional tip percentage (represented as a float, e.g. .2 = 20% tip). Return a tuple with the total bill amount and the tip amount (if included).
 
+
+func amount (amount: Float, percentage: Float) -> (check: Double, tip: Double) {
+    let tipAmount = amount * percentage
+    let totalBill = amount + tipAmount
+    
+    return (Double(totalBill), Double(tipAmount))
+}
+
+
+
 // TODO: Write a function that takes in a string and returns a string that is the reverse of the input. Append two strings using the + operator.
+
+var word: String = "Scott"
+func reverseString (word: String) {
+    print(reverse(word))
+}
 
 // BONUS TODO: Write a function that takes in an array of strings and a search term string. Return a boolean indicating whether the search term string exists in the array.
 
--- Homework/HW 2 Oct 11/Functions notes.playground/section-1.swift
+++ Homework/HW 2 Oct 11/Functions notes.playground/section-1.swift
@@ -62,11 +62,20 @@ Final note: don't be afraid to have long function names, they'll help you in big
 // Create a function knockknock that just prints the line "Who's there?"
 // This function has no parameters nor a return value
 
+func knockknock () {
+    print("Who's there?")
+}
 
 // That was fun! Let's joke around and knock a few times more
 // Write a function knockMultipleTimes that accepts one paramter
 // That parameter will be an int that let's us specify how many times we want to knock
 // Within the function we have a loop that runs x ammount of times that calls the knock knock function
+
+func knockMultipleTimes(knocks: Int) {
+    for _ in 1...knocks {
+        print(knockknock())
+    }
+}
 
 
 // HAHAHA, okay let's answer before the person behind the door goes nuts!
@@ -74,6 +83,9 @@ Final note: don't be afraid to have long function names, they'll help you in big
 // Parameter one will be a firstname (String), and parameter two will be the homeCountry (also String)
 // The function will just print "It's me, {firstname}, from {homeCountry}"
 
+func replyWhoIsHere(firstName: String, homeCountry: String) {
+    print("It's me, \(firstName) from \(homeCountry)")
+}
 
 // Cool that worked! Let's say we want to reply with our age instead of home country!
 // We'll create a function calculateAge that accepts one parameter that will be yearOfBirth (Int)
@@ -89,16 +101,40 @@ let age = currentYear - yearOfBirth
 // Return the age at the end of the function
 
 
+func calculateAge(yearOfBirth: Int) -> Int {
+    let currentDate = NSDate()
+    let currentCalendar = NSCalendar.currentCalendar()
+    let currentYear = 2015
+    let age = currentYear - yearOfBirth
+    return age
+}
+calculateAge(1993)
+
 // Create a function tellNameAndAge that has two parameters
 // Parameter one is the name (String), parameter two is the yearOfBirth(Int)
 // The function will have one var: age, you will call the calculateAge function and store the return value in that var
 // After that you will print a line that states "{name}, my age is {age}", where {age} is the var of course!
 
+func tellNameAndAge (name: String, yearOfBirth: Int) -> Int {
+    var age = calculateAge(1993)
+    return age
+}
+tellNameAndAge("Scott", yearOfBirth: 1993)
+
 
 // Call knock knock
 
+knockknock()
+
+
 // Call knockMultipleTimes
+
+knockMultipleTimes(7)
+
 
 // Call replyWhoIsHere
 
+replyWhoIsHere("scott", homeCountry: "USA")
+
 // Call tellNameAndAge
+
